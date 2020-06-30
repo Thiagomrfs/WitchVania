85264cf80742eec1b2204278914b4e25ec364f59
Thiagomrfs <thiagomarbsn@gmail.com>
2020-06-30 15:58:03 -0300

    iniciado o jogo

---
+++ personagens/enemy.gd
@@ -0,0 +1,99 @@
+extends KinematicBody2D
+
+const GRAVITY = 10
+const FLOOR = Vector2(0, -1)
+const MANABALL = preload("res://mecanicas/manaball.tscn")
+
+var motion = Vector2()
+var is_dead = false
+var is_boss_dead = false
+onready var attack = get_node("attack")
+onready var attack_delay = get_node("manabal_delay")
+var is_attacking = false
+var jump_power = -250
+var boss_hp = 5
+
+export(int) var direction = 1
+export(int) var speed = 60
+export(String) var type = "default"
+export(bool) var boss = false
+
+func _ready():
+	if boss:
+		scale = Vector2(2, 2)
+	if type == "shooter":
+		attack.set_wait_time(3.5)
+		attack.start()
+		
+		if direction == -1:
+				$AnimatedSprite.flip_h = true
+				$Position2D.position *= -1 
+		else:
+			$AnimatedSprite.flip_h = false
+
+func death():
+	if boss:
+		boss_hp -= 1
+		if boss_hp <= 0:
+			is_dead = true
+			is_boss_dead = true
+			motion = Vector2(0, 0)
+			$AnimatedSprite.play("death")
+			$CollisionShape2D.set_deferred("disabled", true)
+	else:
+		is_dead = true
+		motion = Vector2(0, 0)
+		$AnimatedSprite.play("death")
+		$CollisionShape2D.set_deferred("disabled", true)
+
+func shoot_manaball():
+	if is_dead == false:
+		var manaball = MANABALL.instance()
+		if sign($Position2D.position.x) == 1:
+			manaball.set_manaball_direction(1)
+		else:
+			manaball.set_manaball_direction(-1)
+		get_parent().add_child(manaball)
+		manaball.position = $Position2D.global_position
+
+func _process(delta):
+	if is_dead == false:
+		if type == "default":
+				if direction == -1:
+					$AnimatedSprite.flip_h = true
+				else:
+					$AnimatedSprite.flip_h = false
+				
+				$AnimatedSprite.play("walking")
+				
+				motion.y += GRAVITY
+				
+				if is_on_wall():
+					direction *= -1
+					$RayCast2D.position.x *= -1
+				elif $RayCast2D.is_colliding() == false:
+					direction *= -1
+
+				motion.x = speed * direction
+				motion = move_and_slide(motion, FLOOR)
+
+				if get_slide_count() > 0:
+					for slide in range(get_slide_count()):
+						if "player" in get_slide_collision(slide).collider.name:
+							get_slide_collision(slide).collider.death()
+		
+		if type == "shooter":
+			if is_attacking == false:
+				$AnimatedSprite.play("idle")
+			else:
+				$AnimatedSprite.play("attacking")
+
+func _on_attack_timeout():
+	attack_delay.set_wait_time(0.6)
+	attack_delay.start()
+	is_attacking = true
+
+func _on_manabal_delay_timeout():
+	shoot_manaball()
+	attack_delay.stop()
+	is_attacking = false
---
+++ personagens/player.gd
@@ -0,0 +1,89 @@
+extends KinematicBody2D
+
+const GRAVITY = 10
+const FLOOR = Vector2(0, -1)
+const MANABALL = preload("res://mecanicas/manaball.tscn")
+
+var motion = Vector2()
+var velocity = 100
+var jump_power = -250
+var is_attacking = false
+var is_dead = false
+var is_attack_boosted = false
+
+onready var timer = get_node("Timer")
+onready var death_timer = get_node("Timer2")
+
+
+func death():
+	is_dead = true
+	motion = Vector2(0,0)
+	$AnimatedSprite.play("die")
+	$CollisionShape2D.set_deferred("disabled", true)
+	death_timer.set_wait_time(3)
+	death_timer.start()
+
+func shoot_manaball():
+	if is_dead == false:
+		var manaball = MANABALL.instance()
+		if sign($Position2D.position.x) == 1:
+			manaball.set_manaball_direction(1)
+		else:
+			manaball.set_manaball_direction(-1)
+		get_parent().add_child(manaball)
+		manaball.position = $Position2D.global_position
+
+func _ready():
+	pass
+
+func _process(delta):
+	if is_dead == false:
+		if Input.is_action_pressed("ui_right"):
+			if is_attacking == false:
+				motion.x = velocity
+				$AnimatedSprite.flip_h = false
+				$AnimatedSprite.play("walking")
+				if sign($Position2D.position.x) == -1:
+					$Position2D.position.x *= -1
+		elif Input.is_action_pressed("ui_left"):
+			if is_attacking == false:
+				$AnimatedSprite.flip_h = true
+				$AnimatedSprite.play("walking")
+				motion.x = -velocity
+				if sign($Position2D.position.x) == 1:
+					$Position2D.position.x *= -1
+		else:
+			motion.x = 0 
+			if is_on_floor() and is_attacking == false:
+				$AnimatedSprite.play("idle")
+		
+		if Input.is_action_just_pressed("ui_up"):
+			if is_on_floor():
+				motion.y = jump_power
+		
+		if Input.is_key_pressed(32) and is_attacking == false:
+			if is_on_floor():
+				motion.x = 0
+			is_attacking = true
+			$AnimatedSprite.play("manaball")
+			timer.set_wait_time(0.6)
+			timer.start()
+	
+		motion.y += GRAVITY
+		motion = move_and_slide(motion, FLOOR)
+	
+		if get_slide_count() > 0:
+			for slide in range(get_slide_count()):
+				if "enemy" in get_slide_collision(slide).collider.name:
+					death()
+
+func _on_AnimatedSprite_animation_finished():
+	is_attacking = false
+
+func _on_Timer_timeout():
+	shoot_manaball()
+	timer.stop()
+
+func _on_Timer2_timeout():
+	get_tree().change_scene("res://fases/Main_menu.tscn")
+	death_timer.stop()
